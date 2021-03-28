6a2f6dc2f3cf77c68ce4ee365230ff84a2db12bb
89032100 <q89032100@gmail.com>
2021-03-28 22:51:57 +0800

    Upload the whole game and Readme.txt

---
+++ 1072041/1072041/Form1.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace _1072041
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(21, 63);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 96);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(123, 63);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(96, 96);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(225, 63);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(96, 96);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(21, 165);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(96, 96);
+            this.button4.TabIndex = 3;
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(123, 165);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(96, 96);
+            this.button5.TabIndex = 4;
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(225, 165);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(96, 96);
+            this.button6.TabIndex = 5;
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(21, 267);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(96, 96);
+            this.button7.TabIndex = 6;
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(123, 267);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(96, 96);
+            this.button8.TabIndex = 7;
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(225, 267);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(96, 96);
+            this.button9.TabIndex = 8;
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(133, 369);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(75, 23);
+            this.button10.TabIndex = 9;
+            this.button10.Text = "重玩";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "輪到";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Green;
+            this.label2.Location = new System.Drawing.Point(59, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 12);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "O";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(90, 23);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(17, 12);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "了";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(225, 10);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(25, 12);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "X：";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(225, 33);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(25, 12);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "O：";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(257, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(9, 12);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "-";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(257, 33);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(9, 12);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "-";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(347, 406);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "圈圈叉叉";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ 1072041/1072041/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _1072041
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            for (int i = 1; i <= 9; i++)
+            {
+                Button btn = (Button)Controls.Find("button" + i, true)[0];
+                btn.Click += new System.EventHandler(btn_Click);
+                btn.Font = new System.Drawing.Font("微軟正黑體", 20.2F, FontStyle.Bold, GraphicsUnit.Point, ((byte)(136)));
+            }
+            label2.Font = new System.Drawing.Font("微軟正黑體", 20.2F, FontStyle.Bold, GraphicsUnit.Point, ((byte)(136)));
+        }
+        private bool b;
+        private bool win;
+        int x = 0;
+        int o = 0;
+        void btn_Click(object sender, EventArgs e)
+        {
+            if (((Button)sender).Text == "")
+            {
+                string who = b ? "X" : "O";
+                ((Button)sender).Text = who;
+                ((Button)sender).ForeColor = b ? Color.Red : Color.Green;
+                label2.Text = b ? "O" : "X";
+                label2.ForeColor = b ? Color.Green : Color.Red;
+                if (Whowin(who))
+                {
+                    MessageBox.Show(who + "贏了");
+                    if (b) label6.Text = Convert.ToString(++x);
+                    else label7.Text = Convert.ToString(++o);
+
+                    Clean();
+                }
+                if (Peace())
+                {
+                    MessageBox.Show("平手");
+                    Clean();
+                }
+                b = !b;
+            }
+        }
+        private bool Peace()
+        {
+            bool space = false;
+            for (int i = 1; i <= 9; i++)
+            {
+                Button btn = (Button)Controls.Find("button" + i, true)[0];
+                if (btn.Text == "") space = true;
+            }
+            return space ? false : true;
+        }
+        private bool Whowin(string who)
+        {
+            win = false;
+            bool[] now = new bool[9];
+            for (int i = 1; i <= 9; i++)
+            {
+                Button btn = (Button)Controls.Find("button" + i, true)[0];
+                if (btn.Text == who)
+                {
+                    now[i - 1] = true;
+                }
+            }
+            for (int i = 0; i <= 7; i++)
+            {
+                int count = 0;
+                for (int j = 0; j <= 2; j++)
+                {
+                    if (now[arr[i, j]]) count++;
+                }
+                if (count == 3)
+                {
+                    for (int j = 0; j <= 2; j++)
+                    {
+                        Button btn = (Button)Controls.Find("button" + (arr[i, j] + 1), true)[0];
+                        btn.ForeColor = Color.Wheat;
+                        btn.Font = new System.Drawing.Font("微軟正黑體", 20.2F, FontStyle.Bold, GraphicsUnit.Point, ((byte)(136)));
+                        btn.BackColor = Color.Red;
+                    }
+                    win = true;
+                    break;
+                }
+            }
+            return win ? true : false;
+        }
+        private void Clean()
+        {
+            for (int i = 1; i <= 9; i++)
+            {
+                Button btn = (Button)Controls.Find("button" + i, true)[0];
+                btn.Text = "";
+                btn.BackColor = SystemColors.Control;
+                btn.UseVisualStyleBackColor = true;
+            }
+        }
+        private void button10_Click(object sender, EventArgs e)
+        {
+            Clean();
+        }
+
+        private int[,] arr = new int[,] { { 0, 1, 2 },
+                                          { 3, 4, 5 },
+                                          { 6, 7, 8 },
+                                          { 0, 3, 6 },
+                                          { 1, 4, 7 },
+                                          { 2, 5, 8 },
+                                          { 0, 4, 8 },
+                                          { 2, 4, 6 } };
+    }
+}
